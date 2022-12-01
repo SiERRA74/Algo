0811ef6989052fb5b006eef4ecdd0c3ae5be4d36
SiERRA74 <75257452+SiERRA74@users.noreply.github.com>
2022-12-01 10:41:24 +0100

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -1,14 +1,29 @@
 #tri par insertion
-tablo = [4,3,-12,12,10,15,2]
+import time
+import random
+
+def listgenerator(n):
+    l = []
+    for i in range(n):
+        a = random.randint(-1000,10000)
+        l.append(a)
+    return l
+        
+randomlist = listgenerator(1000)
 
 def tri_insertion(liste): 
+    passage = 0
+    start = time.time()
     for i in range(1, len(liste)): 
         Y = liste[i] 
         X = i-1
         while X >= 0 and Y < liste[X] : 
                 liste[X + 1] = liste[X] 
+                passage +=1
                 X -= 1
         liste[X + 1] = Y
-    return liste
+    return ("liste rangé :\n {} \n nombre de passage: {}".format(liste, passage))
+    #return ("liste rangé :\n{} \n time elapsed : {}".format(liste, time.time() - start))
+    
 
-print(tri_insertion(tablo))
+tri_insertion(randomlist)
