d9a70ec745a118da201b0ada4c892f94ce75b035
SiERRA74 <75257452+SiERRA74@users.noreply.github.com>
2022-12-02 10:44:22 +0100

    Update stats3C.py

--- stats3C.py
+++ stats3C.py
@@ -21,27 +21,26 @@ def tri_bulle(liste):
             if liste[i] > liste[i+1]:
                 echange = True
                 liste[i], liste[i+1] = liste[i+1],liste[i]
-                echanges +=1
+                echanges +=3
             comparaison += 1
-    return [comparaison,echanges*3]
+    return comparaison + echanges
   
   
 def tri_selection(liste):
-    #start = time.time()
     comparaison = 0
-    echanges = 0
+    echange = 0
     for i in range(len(liste)):
         mini = i
         for j in range(i+1, len(liste)):
-            echanges += 1
             if liste[mini] > liste[j]:
+                comparaison +=1
                 mini = j
                 
         X = liste[i]
         liste[i] = liste[mini]
         liste[mini] = X
-        comparaison +=1
-    return [comparaison,echanges*3]  
+        echange +=3
+    return comparaison + echange
   
   
 def tri_insertion(liste): 
@@ -53,11 +52,11 @@ def tri_insertion(liste):
         X = i-1
         while X >= 0 and Y < liste[X] : 
                 liste[X + 1] = liste[X] 
-                echanges +=1
+                echanges +=3
                 X -= 1
         liste[X + 1] = Y
         comparaison += 1
-    return [comparaison,echanges*3]
+    return comparaison+echanges
   
 def tri_bulle_opti(liste):
     comparaison = 0
@@ -68,19 +67,24 @@ def tri_bulle_opti(liste):
             if liste[j] > liste[j+1]:
                 liste[j+1], liste[j] = liste[j], liste[j+1]
                 sorted = False
-                echanges +=1
+                echanges +=3
             comparaison +=1
     sorted = True
     if sorted == True:
-        return [comparaison,echanges]
-      
-def stats(mini, maxi, step, lim, tri):
-    for i in range(mini, maxi + step, step):
-        com = 0
-        for n in range(lim):
-            ls = [rd.randint(0, 100) for _ in range(i)]
-            com += tri(ls)
-        print(i, com / lim *0.1)
+        return comparaison+echanges
+     
+
+def stats(mini, maxi, pas, preci,def_tri):
+    for i in range(mini, maxi + pas, pas):
+        start = time.time()
+        cout = 0
+        for n in range(preci):
+            liste = [random.randint(0, 100) for _ in range(i)]
+            cout += def_tri(liste)
+        total = round((cout / preci * 0.1),2) 
+        print("len de liste : {}\ncoût moyen {} \ncout en temps :{}\n".format(i, total , time.time()-start)
+stat(150, 200, 10, 500,tri_selection)
+
   
       
 stats_tri_selec = stats(10,20,5,10,tri_selection())
